edfbce9cc3bc48dc57274f45c88355c1294722a0
Samuel Gray <sam.gray22@gmail.com>
2018-02-14 17:04:30 +0000

    Filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,30 +9,56 @@ let defaultStyle = {
   color: defaultTextColor
 };
 let fakeServerData = {
-  user:{
+  user: {
     name: 'Sam',
-    playlists: [
-      {
+    playlists: [{
         name: "my Faves",
-        songs: [{name:'beat it', duration: "1325"}, {name:'beat it', duration: "1325"}]
+        songs: [{
+          name: 'beat it',
+          duration: "1325"
+        }, {
+          name: 'beat it',
+          duration: "1325"
+        }]
       },
       {
         name: "Discover weekly",
-        songs: [{name:'beat it', duration: "1325"}, {name:'beat it', duration: "1325"}]
+        songs: [{
+          name: 'beat it',
+          duration: "1325"
+        }, {
+          name: 'beat it',
+          duration: "1325"
+        }]
       },
       {
         name: "Prince",
-        songs: [{name:'Purple Rain', duration: "7000"}, {name:'Lets go Crazy', duration: "1325"}]
+        songs: [{
+          name: 'Purple Rain',
+          duration: "7000"
+        }, {
+          name: 'Lets go Crazy',
+          duration: "1325"
+        }]
       }
     ]
   }
 };
 class PlaylistCounter extends Component {
   render() {
-    return (
-      <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
-        <h2>{this.props.playlists.length} playlists</h2>
-      </div>
+    return ( <
+      div style = {
+        { ...defaultStyle,
+          width: "40%",
+          display: "inline-block"
+        }
+      } >
+      <
+      h2 > {
+        this.props.playlists.length
+      }
+      playlists < /h2> < /
+      div >
     );
   }
 }
@@ -41,67 +69,88 @@ class HoursCounter extends Component {
     }, []);
     let totalDuration = allSongs.reduce((sum, eachSong) => {
       return sum + eachSong.duration
-  },0);
+    }, 0);
     return (
-      <div style={{...defaultStyle, width: "40%", display: "inline-block"}}>
-        <h2>{Math.round(totalDuration/60)} hours</h2>
-      </div>
+      <div style =
+        {
+          { ...defaultStyle,
+            width: "40%",
+            display: "inline-block"
+          }
+        } >
+        <h2 > {Math.round(totalDuration / 60)} hours</h2>
+       </div>
     );
   }
 }
 
 class Filter extends Component {
-  render(){
-    return(
-      <div style={defaultStyle}>
-        <img/>
-        <input type="text"/>
+  render() {
+    return (
+      <div style = {defaultStyle}>
+      <img / >
+      <input type = "text" onKeyUp={event =>
+        this.props.onTextChange(event.target.value)}/ >
       </div>
     );
   }
 }
 class Playlist extends Component {
-  render(){
+  render() {
     let playlist = this.props.playlist;
-    return(
-      <div style={{...defaultStyle, display: "inline-block", width: "20%"}}>
-        <img/>
-        <h3>{playlist.name}</h3>
-          <ul>
-            {playlist.songs.map(song =>
-              <li>{song.name}</li>
-            )}
-          </ul>
-      </div>
+    return (
+      <div style = {
+        { ...defaultStyle,
+          display: "inline-block",
+          width: "20%"
+        }
+      }>
+      <img/>
+      <h3> {playlist.name}</h3>
+      <ul> {playlist.songs.map(song =>
+          <li> {song.name}</li>
+        )
+      } </ul>
+      </div >
 
     );
   }
 }
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {serverData: {}}
+    this.state = {
+      serverData: {},
+      filterString: ''
+    }
   }
-  componentDidMount(){
-      this.setState({serverData: fakeServerData});
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        serverData: fakeServerData
+      });
+    }, 1000);
+
 
   }
   render() {
     return (
-      <div className="App">
-        {this.state.serverData.user ?
-          <div>
-            <h1>{this.state.serverData.user.name}s Playlist</h1>
-            <PlaylistCounter playlists= {this.state.serverData.user.playlists} />
-            <HoursCounter playlists= {this.state.serverData.user.playlists} />
-            <Filter />
-            {this.state.serverData.user.playlists.map(playlist =>
-              <Playlist playlist={playlist} />
-            )}
+      <div className = "App" > {
+        this.state.serverData.user ?
+        <div>
+        <h1> {this.state.serverData.user.name} s Playlist </h1>
+        <PlaylistCounter playlists = {this.state.serverData.user.playlists}/>
+        <HoursCounter playlists = {this.state.serverData.user.playlists}/>
+        <Filter onTextChange={text => this.setState({filterString: text})}/>
+        {this.state.serverData.user.playlists.filter(playlist =>
+          playlist.name.toLowerCase().includes(this.state.filterString.toLowerCase())
+        ).map(playlist =>
+          <Playlist playlist = {playlist}/>)
+        }
           </div> : "loading..."
         }
-      </div>
+        </div>
     );
   }
 }
